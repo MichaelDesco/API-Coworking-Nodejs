e9323cb1d9315025478082ce0d2740ef7172a33d
Michaël Descorcier <michael.descorcier@lapiscine.pro>
2023-04-04 09:40:57 +0200

    add-api

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// je crée une variable qui utilise la méthode require pour importer le fichier mock-coworkings.js
 const express = require('express')
 const app = express()
 const port = 3108
 
-app.get('/MyRoute', (req, res) => {
-    console.log('I make a request to the server')
-    res.send('Hello World!')
+const coworkings = require('./mock-coworkings');
+
+app.get('/api/coworkings', (req, res) => {
+  const myCoworking = coworkings.filter((data) => {
+    return data.superficy > req.query.limit
+    })
+  res.json(myCoworking)
 })
 
+app.get('/api/coworkings/:id', (req, res) => {
+  let myCoworking = coworkings.find((data) => {
+    return data.id === parseInt(req.params.id)
+  })
+  res.json(myCoworking)
+})
+
+// j'utilise listen pour écouter le port 3108
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App on port ${port}`)
 })
